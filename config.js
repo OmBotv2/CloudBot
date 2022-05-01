ea9c029dcac33265b3a95ae22d906f9b4da71d32
OmBotv2 <103894626+OmBotv2@users.noreply.github.com>
2022-05-02 07:38:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281224863098'],
+  ['6282239202895'],
   ['0'],
-  ['6281224863098', 'Della', true]
+  ['6282239202895', 'Della', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'Created By'
-global.author = 'Cloud Bot'
+global.author = 'by Hikari Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
