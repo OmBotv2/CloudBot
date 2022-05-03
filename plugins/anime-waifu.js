f645233cb995c2d0b92b2a8a1ae147ba898343ce
OmBotv2 <103894626+OmBotv2@users.noreply.github.com>
2022-05-04 01:30:23 +0800

    Update anime-waifu.js

--- plugins/anime-waifu.js
+++ plugins/anime-waifu.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'
-    conn.sendButton(m.chat, 'Istri kartun', author, json.url, [['waifu', `${usedPrefix}waifu`]], m)
+    conn.sendButton(m.chat, '𝕁𝔸ℕ𝔾𝔸ℕ 𝕃𝕌ℙ𝔸 𝔻𝕆ℕ𝔸𝕋𝔼 ℕ𝕆𝕄𝕆ℝ 𝔾𝕆ℙ𝔸𝕐 𝔻𝔸ℕ 𝔻𝔸ℕ𝔸 𝕀ℕ𝕀 𝕐𝔸 𝕂𝔸𝕂 6282239202895', author, json.url, [['waifu', `${usedPrefix}waifu`]], m)
 }
 handler.help = ['waifu']
 handler.tags = ['internet']
